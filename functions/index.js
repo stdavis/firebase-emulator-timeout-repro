0f922fe21f2433208cfc911a4030552143e906f8
stdavis <stdavis@utah.gov>
2022-08-12 13:09:19 -0600

    fix: add cors header

--- functions/index.js
+++ functions/index.js
@@ -3,5 +3,7 @@ import functions from 'firebase-functions';
 export const maps = functions.runWith({ invoker: 'public' }).https.onRequest((request, response) => {
   console.log("didn't timeout");
 
+  response.setHeader('Access-Control-Allow-Origin', '*');
+
   return response.status(200).send('Hello from Firebase!');
 });
